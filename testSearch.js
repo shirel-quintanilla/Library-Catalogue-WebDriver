6ba2c734c111fe1b704d0f3848d085bc3ad05930
Douglas PG <pineda2003d@gmail.com>
2025-01-27 15:14:53 -0500

    webdriver takes console input and looks for keyword in catalog

--- testSearch.js
+++ testSearch.js
@@ -1,28 +1,28 @@
-const { Builder, By, until } = require("selenium-WebDriver");
+// Require selenium webdriver 
+const { Builder, By, until } = require("selenium-webdriver");
+let webdriver = require("selenium-webdriver"); 
+//Require promptsync
+let prompt = require('prompt-sync')();
 
-const assert = require("assert");
+// Require webdriver for chrome 
+// browser called chromedriver 
+// NOTE FOR LATER: BUT WHAT IF USER IS NOT IN CHROME? 
+// NOTE FOR LATER: MAKE INTO ONE ASYNC FUNCTION INSETAD OF THIS !!!
+require("chromedriver"); 
 
-async function loginTest() {
-// launch the browser
-let driver = await new Builder().forBrowser("chrome").build();
-try {
-  //navigate to facebook login page
-  await driver.get("https://test-login-app.vercel.app/");
-  // Select input elements and fill them out
-  await driver.findElement(By.id("email")).sendKeys("test3@gmail.com");
-  await driver.findElement(By.id("password")).sendKeys("Password@12345");
-  // Select login button and invoke click action
-  //If login details are correct we wiil be redirected to the welcome page
-  await driver.findElement(By.name("login")).click();
-  //On succesful login get page title
-  //Check page title, to confirm login was successful
-  const pageTitle = await driver.getTitle();
-  // assert usign node assertion
-  assert.strictEqual(pageTitle, "Welcomepage");
-  //Check if redirect to login page was successfull
-  await driver.wait(until.titleIs("Welcomepage"), 4000);
-} finally {
-  await driver.quit();
-}
-}
-loginTest();
+// Take in the input of the search 
+var searchtext = prompt("insert search term:");
+//var inputtype = prompt("select one: 'Keyword', 'Title', 'Start of Title', 'Series', 'Author', 'Subject', 'Call Number'");
+//var searchplace = prompt("select one: 'in Library Catalog', 'in List', 'in History & Archives'"); 
+// Build new window of chrome 
+let driver = new webdriver.Builder().forBrowser("chrome").build(); 
+
+// Open catalogue using get method 
+driver.get("https://willmstwn.cwmars.org/?browseCategory=main_new_non_fiction");
+// Insert user's search term in cataloge's search bar
+driver.findElement(By.id("lookfor")).sendKeys(searchtext);
+// Find ancestor of search button 
+const searchsearch = driver.findElement(By.id("horizontal-search-button-container"));
+//Find search and click it
+searchsearch.findElement(By.className("btn btn-default")).click();
+
